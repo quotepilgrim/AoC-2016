e4deb33fedcc204acfaa5213ebd0da774237f761
Quote Pilgrim <caio.psv@gmail.com>
2026-02-25 12:54:34 -0300

    having some fun with how the result is presented

--- main.lua
+++ main.lua
@@ -1,5 +1,24 @@
 local result, day
 local argv = {}
+local drops = {}
+local random = love.math.random
+local max = math.max
+local sqrt = math.sqrt
+
+local function reset_drop(drop, init)
+	if init then
+		drop.w = love.graphics.getFont():getWidth(result)
+		drop.h = love.graphics.getFont():getHeight()
+		drop.y = random(0, 600)
+	else
+		drop.y = -drop.h
+	end
+
+	drop.x = random(-drop.w, 800)
+	drop.speed = random() * 400
+	drop.opacity = drop.speed / 400
+	drop.scale = max(0.15, drop.opacity ^ 2)
+end
 
 function love.load(_, arg)
 	while #arg > 0 do
@@ -46,7 +65,7 @@ function love.load(_, arg)
 	end
 
 	love.window.setTitle(love.window.getTitle() .. " - Day " .. argv.d .. " Part " .. part)
-	love.graphics.setFont(love.graphics.newFont(24))
+	love.graphics.setFont(love.graphics.newFont(48))
 
 	part = part:sub(1, 1):match("%d") == nil and part or "p" .. part
 	filename = filename or ("inputs/d" .. argv.d .. ".txt")
@@ -58,6 +77,12 @@ function love.load(_, arg)
 		result = "???"
 	end
 
+	for _ = 1, 250 do
+		local drop = {}
+		reset_drop(drop, true)
+		table.insert(drops, drop)
+	end
+
 	if argv.v or argv.verbose then
 		for k, v in pairs(argv) do
 			print(k, v)
@@ -67,7 +92,19 @@ function love.load(_, arg)
 end
 
 function love.draw()
-	love.graphics.print(result, 8, 8)
+	for _, drop in ipairs(drops) do
+		love.graphics.setColor(1, 1, 1, drop.opacity)
+		love.graphics.print(result, drop.x, drop.y, 0, drop.scale)
+	end
+end
+
+function love.update(dt)
+	for _, drop in ipairs(drops) do
+		drop.y = drop.y + drop.speed * dt
+		if drop.y > 600 then
+			reset_drop(drop)
+		end
+	end
 end
 
 function love.keypressed(key)
