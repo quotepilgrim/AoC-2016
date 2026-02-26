a1831b9ca14b47864e421682a3dc21f7a0e8683c
Quote Pilgrim <caio.psv@gmail.com>
2026-02-26 00:57:31 -0300

    d7 done

--- d7.lua
+++ d7.lua
@@ -11,7 +11,7 @@ local function load_data(file)
 	return data
 end
 
-local function find_abba(s)
+local function separate(s)
 	local pattern = "%[[^%[%]]+%]"
 	local outside = s:gsub(pattern, "[]")
 	local inside = ""
@@ -20,7 +20,12 @@ local function find_abba(s)
 		inside = inside .. match
 	end
 
+	return inside, outside
+end
+
+local function find_abba(s)
 	local abba_out, abba_in
+	local inside, outside = separate(s)
 
 	for i = 1, #outside - 3 do
 		local a1, b1, b2, a2 = outside:sub(i, i + 3):match("(.)(.)(.)(.)")
@@ -41,12 +46,43 @@ local function find_abba(s)
 	return abba_out and not abba_in
 end
 
+local function find_aba(s)
+	local inside, outside = separate(s)
+
+	for i = 1, #outside - 2 do
+		local a1, b, a2 = outside:sub(i, i + 2):match("(.)(.)(.)")
+		if a1 ~= b and a1 == a2 then
+			for j = 1, #inside - 2 do
+				local match = inside:sub(j, j + 2)
+				if match == b .. a1 .. b then
+					return true
+				end
+			end
+		end
+	end
+
+	return false
+end
+
 function M.p1(file)
 	local data = load_data(file)
 	local result = 0
 
 	for _, s in ipairs(data) do
 		if find_abba(s) then
+			result = result + 1
+		end
+	end
+
+	return result
+end
+
+function M.p2(file)
+	local data = load_data(file)
+	local result = 0
+
+	for _, s in ipairs(data) do
+		if find_aba(s) then
 			result = result + 1
 		end
 	end
